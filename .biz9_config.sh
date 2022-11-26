73139e4e96660ad070adc82db085303ad53c32cd
tank <tank@certifiedcoderz.com>
2022-11-26 16:06:32 -0500

    good

--- .biz9_config.sh
+++ .biz9_config.sh
@@ -1,5 +1,5 @@
 BIZ9_VENDOR_VERSION='3.6.5'
-APP_VERSION='1.8.7'
+APP_VERSION='1.8.8'
 APP_TITLE="DOQBox-Keyboard-Shortcutz";
 GIT_BRANCH=main
 REPO_URL="git@github.com:bossappz/bossappz-doqbox_keyboard_shortcutz.git";
